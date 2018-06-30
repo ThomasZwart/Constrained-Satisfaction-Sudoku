176c5854383e8e39000b262f34c1e62551c828a5
ThomasZwart <36655280+ThomasZwart@users.noreply.github.com>
2018-06-30 23:53:16 +0200

    Working version

--- Constrained-Satisfaction-Sudoku_/Constrained-Satisfaction-Sudoku/Program.cs
+++ Constrained-Satisfaction-Sudoku_/Constrained-Satisfaction-Sudoku/Program.cs
@@ -8,8 +8,99 @@ namespace Constrained_Satisfaction_Sudoku
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[,] input = ReadSudoku();
+            Sudoku sudoku = new Sudoku(input);
+            Console.WriteLine();
+
+            Sudoku newSudoku = ChronologicalBacktracking(sudoku);
+            newSudoku.WriteSudoku();
+
+
+            Console.ReadLine();            
+        }
+
+        static private Sudoku ChronologicalBacktracking(Sudoku sudoku)
+        {
+            Stack<Sudoku> stack = new Stack<Sudoku>();
+            stack.Push(sudoku);
+
+            while (stack.Count != 0)
+            {
+                int count = sudoku.domainCounter;               
+
+                Sudoku newSudoku = sudoku.Clone();
+
+                if (newSudoku.FirstEmptyVariable() == null)
+                {
+                    break;
+                }
+                // Generate successor
+                newSudoku.domainCounter = 0;
+                Tuple<int, int> newVariable = newSudoku.FirstEmptyVariable();
+                List<int> variableDomain = newSudoku.domains[newVariable.Item1, newVariable.Item2];
+                newSudoku.grid[newVariable.Item1, newVariable.Item2] = variableDomain[count];
+                stack.Push(newSudoku);
+
+                sudoku.domainCounter++; // Domain counter goes up
+
+                // Failed partial solution
+                if (!newSudoku.IsPartialSolution(newVariable))
+                {
+                    stack.Pop();
+                }
+
+                // Backtrack step
+                if (sudoku.domainCounter >= variableDomain.Count)
+                {
+                    sudoku = stack.Pop();
+                }
+            }
+
+            return sudoku;
+        }
+
+        private static int[,] ReadSudoku()
+        {
+            try
+            {
+                // Get the length of the first row N, the sudoku size is N * N 
+                char[] firstRowArray = Console.ReadLine().Trim().ToCharArray();
+
+                int sudokuLength = firstRowArray.Length;
+                int[,] sudoku = new int[sudokuLength, sudokuLength];
+                string[] allRows = new string[sudokuLength];
+
+                // Add the first row to the sudoku array
+                for (int i = 0; i < firstRowArray.Length; i++)
+                {
+                    sudoku[0, i] = int.Parse(firstRowArray[i].ToString());
+                }
+
+                // Add the rest of the rows to the sudoku array
+                for (int i = 1; i < sudokuLength; i++)
+                {
+                    string s = Console.ReadLine().Trim();
+                    char[] rowCharArray = s.ToCharArray();
+
+                    for (int j = 0; j < rowCharArray.Length; j++)
+                    {
+                        sudoku[i, j] = int.Parse(rowCharArray[j].ToString());
+                    }
+                }
+                return sudoku;
+            }
+            catch (Exception e)
+            {
+                // Incorrect input
+                Console.WriteLine(e.Message);
+                //Thread.Sleep(3000); // Wait for user to read error
+                Environment.Exit(0); // Exit console
+                return null;
+            }
         }
     }
+
+
 }
--- Constrained-Satisfaction-Sudoku_/Constrained-Satisfaction-Sudoku/Sudoku.cs
+++ Constrained-Satisfaction-Sudoku_/Constrained-Satisfaction-Sudoku/Sudoku.cs
@@ -1,45 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace SudukoLocalSearch
+namespace Constrained_Satisfaction_Sudoku
 {
     class Sudoku
     {
         public int[,] grid;
-        public HashSet<int>[,] domains;
+        public List<int>[,] domains;
         public HashSet<Tuple<int, int>> fixedNumbers;
         public HashSet<Tuple<Tuple<int, int>, Tuple<int, int>>> constraintSet;
-
-        private static readonly Random getRandom = new Random();
+        public int domainCounter;
 
         public Sudoku(int[,] _sudoku)
         {
             grid = _sudoku;
+            domainCounter = 0;
             SetFixedNumbers();
             InstantiateDomains();
-            InstantiateConstrainSet();
+            InstantiateConstraintSet();
+            DomainReduction();
         }
 
-        private void InstantiateConstrainSet()
+        public void DomainReduction()
+        {
+            for (int row = 0; row < Length; row++)
+            {
+                for (int column = 0; column < Length; column++)
+                {
+                    
+                }
+            }
+        }
+
+        public bool IsPartialSolution(Tuple<int, int> variable)
+        {
+            foreach (Tuple<Tuple<int, int>, Tuple<int, int>> constraint in constraintSet)
+            {
+                if (constraint.Item1.Equals(variable))
+                {
+                    if (grid[variable.Item1, variable.Item2] == grid[constraint.Item2.Item1, constraint.Item2.Item2])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public Tuple<int, int> FirstEmptyVariable()
         {
             for (int i = 0; i < Length; i++)
             {
                 for (int j = 0; j < Length; j++)
                 {
-                    if (!fixedNumbers.Contains(new Tuple<int, int>(i, j)))
+                    if (grid[i, j] == 0)
                     {
-                        for (int row = 0; row < Length; row++)
+                        return new Tuple<int, int>(i, j);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void InstantiateConstraintSet()
+        {
+            constraintSet = new HashSet<Tuple<Tuple<int, int>, Tuple<int, int>>>();
+            int blockLength = (int)Math.Sqrt(Length);
+            for (int blockRow = 0; blockRow < blockLength; blockRow++)
+            {
+                for (int blockColumn = 0; blockColumn < blockLength; blockColumn++)
+                {
+                    int blockIndexRow = blockRow * blockLength;
+                    int blockIndexColumn = blockColumn * blockLength;
+
+                    for (int row = blockIndexRow; row < blockIndexRow + blockLength; row++)
+                    {
+                        for (int column = blockIndexColumn; column < blockIndexColumn + blockLength; column++)
                         {
-                            for (int column = 0; column < Length; column++)
+                            if (!fixedNumbers.Contains(new Tuple<int, int>(row, column)))
                             {
-                                if (i == row)
+                                for (int y = blockIndexRow; y < blockIndexRow + blockLength; y++)
                                 {
-
+                                    for (int x = blockIndexColumn; x < blockIndexColumn + blockLength; x++)
+                                    {
+                                        if (!(row == y && x == column))
+                                        {
+                                            constraintSet.Add(new Tuple<Tuple<int, int>, Tuple<int, int>>(new Tuple<int, int>(row, column), new Tuple<int, int>(y, x)));
+                                        }
+                                    }
                                 }
-                                if (j == column)
-                                {
+                            }
+                        }
+                    }
+                }
+            }
 
-                                }
+            for (int row = 0; row < Length; row++)
+            {
+                for (int column = 0; column < Length; column++)
+                {
+                    if (!fixedNumbers.Contains(new Tuple<int, int>(row, column)))
+                    {
+                        for (int i = 0; i < Length; i++)
+                        {
+                            if (i != column)
+                            {
+                                constraintSet.Add(new Tuple<Tuple<int, int>, Tuple<int, int>>(new Tuple<int, int>(row, column), new Tuple<int, int>(row, i)));
+                            }
+                        }
+
+                        for (int i = 0; i < Length; i++)
+                        {
+                            if (i != row)
+                            {
+                                constraintSet.Add(new Tuple<Tuple<int, int>, Tuple<int, int>>(new Tuple<int, int>(row, column), new Tuple<int, int>(i, column)));
                             }
                         }
                     }
@@ -49,13 +126,18 @@ namespace SudukoLocalSearch
 
         private void InstantiateDomains()
         {
+            domains = new List<int>[Length, Length];
             for (int i = 0; i < Length; i++)
             {
                 for (int j = 0; j < Length; j++)
                 {
                     if (!fixedNumbers.Contains(new Tuple<int, int>(i, j)))
                     {
-                        domains[i, j] = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                        domains[i, j] = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+                    }
+                    else
+                    {
+                        domains[i, j] = new List<int> { grid[i, j]};
                     }
                 }
             }
@@ -67,7 +149,18 @@ namespace SudukoLocalSearch
             Sudoku clone = new Sudoku(grid.Clone() as int[,])
             {
                 fixedNumbers = new HashSet<Tuple<int, int>>(fixedNumbers)
+
             };
+            clone.domainCounter = domainCounter;
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    clone.domains[i, j] = new List<int>(domains[i, j]);
+                }
+            }
+            clone.constraintSet = new HashSet<Tuple<Tuple<int, int>, Tuple<int, int>>>(constraintSet);
+
             return clone;
         }
 
